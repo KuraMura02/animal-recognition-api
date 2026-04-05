34a1a9a03860f85d6717554a907b123fc39fa3f0
Kuralai <115729885+KuraMura02@users.noreply.github.com>
2026-04-05 14:49:50 +0500

    Update predict.py

--- utils/predict.py
+++ utils/predict.py
@@ -10,15 +10,17 @@ classes = None
 def load_model_once():
     global model, classes
     if model is None:
-        model = tf.keras.models.load_model("model/model_mobilenet.h5")
-        classes = [line.strip() for line in open("classes.txt")]
+        # compile=False убирает проблемы с параметрами BatchNormalization
+        model = tf.keras.models.load_model("model/model_mobilenet.h5", compile=False)
+        with open("classes.txt", "r", encoding="utf-8") as f:
+            classes = [line.strip() for line in f]
 
 def predict_animal(image_bytes):
     load_model_once()
 
     img = Image.open(io.BytesIO(image_bytes)).convert('RGB')
-    img = img.resize((224,224))
-    x = np.array(img)/255.0
+    img = img.resize((224, 224))
+    x = np.array(img, dtype=np.float32) / 255.0
     x = np.expand_dims(x, axis=0)
 
     pred = model.predict(x)
